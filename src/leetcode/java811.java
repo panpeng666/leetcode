4bf9f65edfe4e97e5b9a35e58bfe75b1746b0b6b
panpeng666 <40056258+panpeng666@users.noreply.github.com>
2019-08-30 14:02:57 +0800

    test

--- src/leetcode/java811.java
+++ src/leetcode/java811.java
@@ -1,4 +1,14 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class java811 {
+
+    class Solution {
+        public List<String> subdomainVisits(String[] cpdomains) {
+            List<String> result = new ArrayList<>();
+            return result;
+        }
+    }
 }
