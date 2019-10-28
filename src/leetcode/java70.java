7e064443619a02b2e1517ddd69abea9a86a38035
panpeng666 <40056258+panpeng666@users.noreply.github.com>
2019-10-28 16:02:24 +0800

    提交新的解题答案 java70

--- src/leetcode/java70.java
+++ src/leetcode/java70.java
@@ -27,5 +27,26 @@ package leetcode;
  * 链接：https://leetcode-cn.com/problems/climbing-stairs
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class java70_not_finish {
+public class java70{
+    //递归算法的话，耗时太长
+    public int climbStairs(int n) {
+        if (n==0||n==1){return 1;}
+        else {return climbStairs(n-1)+climbStairs(n-2);}
+    }
+
+    //官方推荐走动态规划
+    public int climbStairs1(int n) {
+        if (n==0||n==1){return 1;}
+        else {
+                int i1=1;
+                int i2=2;
+            int temp=0;
+            for (int i=3;i<=n;i++){
+                temp=i1+i2;
+                i2=temp;
+                i1=i2;
+            }
+            return temp;
+        }
+    }
 }
