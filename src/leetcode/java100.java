078c8037b6e799deda3f47054ee1aae983c45c29
panpeng <panpeng@guahao.com>
2021-05-25 10:26:26 +0800

    提交100号刷题

---
+++ src/leetcode/java100.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Objects;
+
+public class java100 {
+    public  class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Solution {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p==null&&q==null)return true;
+            if (p==null&&q!=null)return false;
+            if (q==null&&p!=null)return false;
+            if (p.val!=q.val){
+                return false;
+            }
+            return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        }
+
+
+    }
+}
